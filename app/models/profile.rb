2ab7a6645a6c31b0bd4595941a9b45785e06a0dd
mskim <mskimsid@gmail.com>
2018-05-02 11:40:35 +0900

    fix profile_rb add cmyk

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -30,23 +30,23 @@ class Profile < ApplicationRecord
   def layout_erb
     layout =<<~EOF
     RLayout::Container.new(width:158.737,  height: 75) do
-      rect(x: 0, y: 10, width:158.737, height: 65,  fill_color:"CMYK=0,0,0,10")
+      rect(x: 0, y: 10, width:158.737, height: 65, fill_color:"CMYK=0,0,0,10")
       image(local_image: '<%= name %>.eps', from_right: 0, y: 0, width: 60, height: 75, fill_color: 'clear')
       container(x: 0, y: 20, width:100, bottom_margin: 10, fill_color: 'clear') do
         <% if name && work && work != "" && position && position != "" %>
           <% if name.include?('-') %>
-          text('<%= name.split("-").first.gsub("+", " ") %>', text_alignment: 'right', from_right: 10, y:17, font: 'KoPubDotumPB', font_size: 9, fill_color: 'clear')
+          text('<%= name.split("-").first.gsub("+", " ") %>', text_alignment: 'right', from_right: 10, y:17, font: 'KoPubDotumPB', font_size: 9, font_color:"CMYK=0,0,0,10", fill_color: 'clear')
           <% else  %>
-          text('<%= name.gsub("+", " ") %>', text_alignment: 'right', from_right: 10, y:17, font: 'KoPubDotumPB', font_size: 9, fill_color: 'clear')
+          text('<%= name.gsub("+", " ") %>', text_alignment: 'right', from_right: 10, y:17, font: 'KoPubDotumPB', font_size: 9,font_color:"CMYK=0,0,0,10", fill_color: 'clear')
           <% end  %>
-          text('<%= work %>', text_alignment: 'right', from_right: 10, y:30, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear')
-          text('<%= position %>', text_alignment: 'right', from_right: 10, y:41, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear')
+          text('<%= work %>', text_alignment: 'right', from_right: 10, y:30, font: 'KoPubDotumPL', font_size: 8, font_color:"CMYK=0,0,0,10", fill_color: 'clear')
+          text('<%= position %>', text_alignment: 'right', from_right: 10, y:41, font: 'KoPubDotumPL', font_size: 8, font_color:"CMYK=0,0,0,10", fill_color: 'clear')
         <% elsif position == "" || position == nil %>
-          text('<%= name.gsub("+", " ") %>', text_alignment: 'right', from_right: 10, y:28, font: 'KoPubDotumPB', font_size: 9, fill_color: 'clear')
+          text('<%= name.gsub("+", " ") %>', text_alignment: 'right', from_right: 10, y:28, font: 'KoPubDotumPB', font_size: 9, font_color:"CMYK=0,0,0,10", fill_color: 'clear')
           text('<%= work %>', text_alignment: 'right', from_right: 10, y:41, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear')
         <% elsif work == "" || work == nil %>
-          text('<%= name.gsub("+", " ") %>', text_alignment: 'right', from_right: 10, y:28, font: 'KoPubDotumPB', font_size: 9, fill_color: 'clear')
-          text('<%= position %>', text_alignment: 'right', from_right: 10, y:41, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear')
+          text('<%= name.gsub("+", " ") %>', text_alignment: 'right', from_right: 10, y:28, font: 'KoPubDotumPB', font_size: 9, font_color:"CMYK=0,0,0,10", fill_color: 'clear')
+          text('<%= position %>', text_alignment: 'right', from_right: 10, y:41, font: 'KoPubDotumPL', font_size: 8, font_color:"CMYK=0,0,0,10", fill_color: 'clear')
         <% end %>
       end
     end
