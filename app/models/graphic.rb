e589e8675dd452e73e2a940ae834d7711a734258
mskim <mskimsid@gmail.com>
2019-01-09 14:46:23 +0900

    fix size_string subtrack 4 from image height

--- app/models/graphic.rb
+++ app/models/graphic.rb
@@ -49,7 +49,7 @@ class Graphic < ApplicationRecord
 
   def size_string
     width_in_cm   = ((working_article.grid_width*column + working_article.gutter*(column-1))*0.352778).round(2)
-    height_in_cm  = ((working_article.grid_height*row + working_article.body_line_height*extra_height_in_lines)*0.352778).round(3)
+    height_in_cm  = ((working_article.grid_height*row + working_article.body_line_height*extra_height_in_lines - 4)*0.352778).round(3)
     "#{width_in_cm}mm x #{height_in_cm}mm"
   end
 
--- app/models/image.rb
+++ app/models/image.rb
@@ -44,9 +44,11 @@ class Image < ApplicationRecord
 
   def size_string
     width_in_cm   = ((working_article.grid_width*column + working_article.gutter*(column-1))*0.352778).round(2)
-    height_in_cm  = ((working_article.grid_height*row + working_article.body_line_height*extra_height_in_lines)*0.352778).round(3)
+    # 4 is value adjustef to align image with body text
+    height_in_cm  = ((working_article.grid_height*row + working_article.body_line_height*extra_height_in_lines - 4)*0.352778).round(3)
     "#{width_in_cm}mm x #{height_in_cm}mm"
   end
+
 
   def publication
     issue.publication
