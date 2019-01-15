f47af16205fb5962b103b61d90182911dfb83b00
smartfty <book@smartfactory.kr>
2019-01-15 14:10:08 +0900

    섹션 템플릿 아이디 검색 기능 추가 외

---
+++ public/1/section/1/7x15_H_7단15_5/25/ad/layout.rb
@@ -0,0 +1,5 @@
+
+RLayout::NewsAdBox.new(is_ad_box: true, column: 3, row: 7, grid_width: 146.99662542182014, grid_height: 97.32283464566807, on_left_edge: false, on_right_edge: true, page_heading_margin_in_lines: 0) do
+  image(image_path: 'some_image_path', layout_expand: [:width, :height])
+  relayout!
+end
