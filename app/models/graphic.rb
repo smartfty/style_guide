5f9edd7410cdc07d132f9e46d16cf1f2aaf9ffe3
smartfty <book@smartfactory.kr>
2019-02-12 10:17:02 +0900

    이미지/그래픽 사이즈 측정 오류 해결

--- app/models/graphic.rb
+++ app/models/graphic.rb
@@ -51,7 +51,7 @@ class Graphic < ApplicationRecord
   def size_string
     width_in_mm   = ((working_article.grid_width*column - working_article.gutter)*0.352778).round(2)
     # 4 is value adjustef to align image with body text
-    extra_height_in_lines = 0 unless extra_height_in_lines
+    # extra_height_in_lines = 0 unless extra_height_in_lines
     height_in_mm  = ((working_article.grid_height*row + working_article.body_line_height*extra_height_in_lines - 4)*0.352778).round(3)
     "#{width_in_mm}mm x #{height_in_mm}mm"
   end
