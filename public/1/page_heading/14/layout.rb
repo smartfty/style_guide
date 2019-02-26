e121f957294ea840f162513b6f1bd54b6b9a0bd6
smartfty <book@smartfactory.kr>
2019-02-27 08:50:47 +0900

    update page plan

--- public/1/page_heading/14/layout.rb
+++ public/1/page_heading/14/layout.rb
@@ -3,5 +3,5 @@ RLayout::Container.new(width: 1028.9763779528, height: 41.70978623622, layout_di
   t = text('산 업', font_size: 20.5, x: 464.0, y: 0.5, width: 100, font: 'KoPubBatangPM', text_color: "CMYK=0,0,0,100", fill_color:'clear', text_fit_type: 'fit_box_to_text', anchor_type: 'center')
   line(x: t.x, y:27.6, width: t.width, stroke_width: 1, height:0, storke_color:"CMYK=0,0,0,100")
   text('14', tracking: -0.2, x: 0, y: -6.47, font: 'Helvetica-Light', font_size: 36, text_color: "CMYK=0,0,0,100", width: 50, height: 44, fill_color: 'clear')
-  text('2018년 9월 14일 금요일', tracking: -0.7, x: 50, y: 12.16, width: 200, font: 'KoPubDotumPL', font_size: 10.5, text_color: "CMYK=0,0,0,100", text_alignment: 'left', fill_color: 'clear')
+  text('0000년 0월 0일 0요일', tracking: -0.7, x: 50, y: 12.16, width: 200, font: 'KoPubDotumPL', font_size: 10.5, text_color: "CMYK=0,0,0,100", text_alignment: 'left', fill_color: 'clear')
 end
