77c498613e9e27510460bd0aa18974578f17eaab
smartfty <book@smartfactory.kr>
2019-03-12 17:47:46 +0900

    section update

--- public/1/section/101/7x15_7단통_4/116/ad/layout.rb
+++
@@ -1,5 +0,0 @@
-
-RLayout::NewsAdBox.new(is_ad_box: true, column: 7, row: 7, grid_width: 146.99662542182014, grid_height: 97.32283464566807, on_left_edge: true, on_right_edge: true, top_position: false, page_heading_margin_in_lines: 0) do
-  image(image_path: 'some_image_path', fit_type: 4, layout_expand: [:width, :height])
-  relayout!
-end
