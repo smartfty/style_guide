77c72db2b0719409033478a9ac63680d83ddecdc
smartfty <book@smartfactory.kr>
2019-03-26 07:30:22 +0900

    opinion / profile update

--- public/1/opinion/정숭호.rb
+++ public/1/opinion/정숭호.rb
@@ -12,7 +12,7 @@ RLayout::Container.new(width:158.74015748031, height: 162.83914494488) do
         text('정숭호', y:17, font: 'KoPubDotumPB', font_size: 9, fill_color: 'clear', text_color:'CMYK=0,0,0,100')
       
       text('언론인', y:30, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear', text_color:'CMYK=0,0,0,100')
-      text('전 한국일보 심의실장', y:41, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear', text_color:'CMYK=0,0,0,100')
+      text('전 신문윤리위원', y:41, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear', text_color:'CMYK=0,0,0,100')
     
   end
 end
