dd943bae87f1de7fc31bc7765efa86a7c468de16
mskim <mskimsid@gmail.com>
2019-04-11 10:25:13 +0900

    add sqeeze to fit mode for picture article

--- app/models/working_article.rb
+++ app/models/working_article.rb
@@ -794,20 +794,27 @@ class WorkingArticle < ApplicationRecord
     "  news_quote(#{quote_hash})\n"
   end
 
-  def layout_rb
+   def layout_rb
     # h = h.to_s.gsub("{", "").gsub("}", "")
     h = layout_options
     if kind == '사진'
-      first_image = images.first
-      h[:draw_frame] = false if first_image && first_image.draw_frame == false
-      content = "RLayout::NewsImageBox.new(#{h}) do\n"
-      if images.length > 0
+      if first_image = images.first
+        h[:draw_frame] = false if first_image && first_image.draw_frame == false
+        content = "RLayout::NewsImageBox.new(#{h}) do\n"
         image_hash = image_options
-        image_hash[:fit_type] = 3 # keep ratio
+        # image_hash[:fit_type] = 3 # keep ratio
         image_hash[:expand] = [:width, :height]
         content += "  news_image(#{image_hash})\n"
+        content += "end\n"
+      elsif first_graphic = graphics.first 
+        h[:draw_frame] = false if first_graphic && first_graphic.draw_frame == false
+        content = "RLayout::NewsImageBox.new(#{h}) do\n"
+        image_hash = first_graphic.graphic_layout_hash
+        # image_hash[:fit_type] = 3 # keep ratio
+        image_hash[:expand] = [:width, :height]
+        content += "  news_image(#{image_hash})\n"
+        content += "end\n"
       end
-      content += "end\n"
     elsif kind == '만평'
       content = "RLayout::NewsComicBox.new(#{h}) do\n"
       if image_hash = image_options
