4e0f0a017670d61c7d4522a8e928b168f5996199
smartfty <33275998+smartfty@users.noreply.github.com>
2019-04-17 08:51:37 +0900

    update

--- public/1/opinion/이정희.rb
+++ public/1/opinion/이정희.rb
@@ -1,4 +1,3 @@
-
 RLayout::Container.new(width:158.74015748031, height: 162.83914494488) do
   line(x: 0 , y: 1.25, width: 158.74015748031, stroke_width: 2, height:0, stroke_color:'CMYK=0,0,0,100')
   text('신문로', x: 0, y:8, font: 'KoPubDotumPB', font_size: 12, width: 170, text_color:'CMYK=0,0,0,100')
@@ -9,10 +8,9 @@ RLayout::Container.new(width:158.74015748031, height: 162.83914494488) do
   container(x: 70, y: 80, width:150, bottom_margin: 10, fill_color: 'clear') do
     
       
-        text('이정희', y:17, font: 'KoPubDotumPB', font_size: 9, fill_color: 'clear', text_color:'CMYK=0,0,0,100')
+        text('이정희', y:28, font: 'KoPubDotumPB', font_size: 9, fill_color: 'clear', text_color:'CMYK=0,0,0,100')
       
-      text('회계사', y:30, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear', text_color:'CMYK=0,0,0,100')
-      text('딜로이트안진 회계법인 대표', y:41, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear', text_color:'CMYK=0,0,0,100')
+      text('회계사', y:41, font: 'KoPubDotumPL', font_size: 8, fill_color: 'clear', text_color:'CMYK=0,0,0,100')
     
   end
 end
