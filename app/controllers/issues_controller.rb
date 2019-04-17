3300395fa2c8bdb7c354eb3490963dca16301bcc
smartfty <33275998+smartfty@users.noreply.github.com>
2019-04-17 11:04:34 +0900

    update issue

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -13,9 +13,7 @@ class IssuesController < ApplicationController
   # GET /issues/1
   # GET /issues/1.json
   def show
-    if session[:current_issue]
-      @issue = session[:current_issue]
-    end
+    session[:current_issue] = @issue
     @pages = @issue.pages.order(:id, 'desc')
     @pages = @issue.pages
     respond_to do |format|
