35876f8642d18bff6f5d378dcc302fdf9e398a2c
smartfty <33275998+smartfty@users.noreply.github.com>
2019-09-09 08:07:16 +0900

    update

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -61,7 +61,7 @@ class Issue < ApplicationRecord
   def set_color_page
     pages.each do |page|
       # puts page.page_number
-      if page.page_number == 22 || page.page_number == 23
+      if page.page_number == 22 || page.page_number == 23 || page.page_number == 10
         page.color_page = false
       else
         page.color_page = true
