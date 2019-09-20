02e15779fe1f0c74ecfe357f5792da2ff2296940
smartfty <33275998+smartfty@users.noreply.github.com>
2019-09-20 12:51:26 +0900

    Section Template update

---
+++ public/1/section/1/7x15_H_4단통_3/325/heading/layout.rb
@@ -0,0 +1,5 @@
+RLayout::Container.new(width: 1028.9763779528, height: 139.0326207874, layout_direction: 'horinoztal') do
+  image(local_image: '1_bg.pdf', x:0, y:0, width: 1028.9763779528, height: 139.0326207874)
+  text('0000년 0월 0일 0요일 (4200호)', x: 828.00, y: 108, fill_color:'clear', width: 200, height: 12, font: 'KoPubDotumPL', font_size: 11, tracking: -0.9, text_color: "CMYK=0,0,0,100", text_alignment: 'right')
+  image(local_image: 'heading_ad.pdf', x:809.137, y:13.043, width: 219.257, height: 71.2)
+end
---
+++ public/1/section/6x15_광고없음_6/314/heading/22/layout.rb
@@ -0,0 +1,5 @@
+RLayout::Container.new(width: 1028.9763779528, height: 55.613048314961, layout_direction: 'horinoztal') do
+  image(local_image: '22_bg.pdf', x: 0, y: 0, width: 1028.9763779528, height: 55.613048314961, fit_type: 0)
+  text('0000년 0월 0일 0요일', x: 864.104, y: 8.8, fill_color: 'clear', tracking: -0.7,  width: 110, height: 11, font: 'KoPubDotumPL', fill_color: 'clear', text_color: "CMYK=0,0,0,100", font_size: 10.5, text_alignment: 'right')
+  text('22', tracking: -0.2, x: 0, y: -4.97, text_alignment: 'center', fill_color: 'clear', font: 'Helvetica-Light', font_size: 36, text_color: "CMYK=0,0,0,100", width: 40, height: 44)
+end
