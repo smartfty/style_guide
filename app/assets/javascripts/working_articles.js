a3368cae69c3db62421157c20f94489bcc4ca961
smartfty <33275998+smartfty@users.noreply.github.com>
2019-10-04 17:16:13 +0900

    update

--- app/assets/javascripts/working_articles.js
+++ app/assets/javascripts/working_articles.js
@@ -6,3 +6,19 @@
 //   $('#myBtn').on('click', function() { $('#edit_working_article_288').submit(); });
 // });
 
+jQuery(function() {
+    var story_subcategory;
+    story_subcategory = $('#working_article_subcategory_code').html();
+    console.log(story_subcategory);
+    return $('#working_article_category_code').change(function() {
+      var category, options;
+      category = $('#working_article_category_code :selected').text();
+      options = $(story_subcategory).filter("optgroup[label=" + category + "]").html();
+      console.log(options);
+      if (options) {
+        return $('#working_article_subcategory_code').html(options);
+      } else {
+        return $('#working_article_subcategory_code').empty();
+      }
+    });
+  });
