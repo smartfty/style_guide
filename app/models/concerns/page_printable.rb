788c9c34a0ce63accfbe6c75c241438504031583
smartfty <33275998+smartfty@users.noreply.github.com>
2019-10-16 08:55:15 +0900

    update

--- app/models/concerns/page_printable.rb
+++ app/models/concerns/page_printable.rb
@@ -262,8 +262,8 @@ module PagePrintable
     yyyymd    = issue.date.strftime("%Y%m%d")
     dir_name  = "/NewsPDF/#{yyyymd}"
     Net::FTP.open(ip, id, pw) do |ftp|
-      files = ftp.list
-      ftp.mkdir(dir_name) if !ftp.list("/").any?{|dir| dir.match(/\s#{dir_name}$/)}
+      # files = ftp.list
+      # ftp.mkdir(dir_name) if !ftp.list("/").any?{|dir| dir.match(/\s#{dir_name}$/)}
         # ftp.mkdir dir_name unless File.exists?(dir_name)
       ftp.putbinaryfile(printer_file, "#{dir_name}/#{news_pdf_code}")
     end
