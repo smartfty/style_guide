00dcc48441486570a4a1fc7aef7f38364d59693c
smartfty <33275998+smartfty@users.noreply.github.com>
2019-11-15 19:43:50 +0900

    섹션 템플릿 머지

--- public/1/section/20/7x15_5단통_4/247/3/layout.rb
+++ public/1/section/20/7x15_5단통_4/247/3/layout.rb
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 RLayout::NewsArticleBox.new({:kind=>"기사", :column=>6, :row=>4, :grid_width=>146.99662542182, :grid_height=>97.3228346456681, :gutter=>12.7559055118108, :on_left_edge=>true, :on_right_edge=>false, :is_front_page=>false, :top_story=>false, :top_position=>false, :bottom_article=>true, :page_heading_margin_in_lines=>0, :article_bottom_spaces_in_lines=>2, :article_line_draw_sides=>"[0, 0, 0, 1]", :article_line_thickness=>0.3, :draw_divider=>nil})
-=======
-RLayout::NewsArticleBox.new({:kind=>"기사", :column=>6, :row=>4, :grid_width=>146.99662542182026, :grid_height=>97.32283464566795, :gutter=>12.755905511810848, :on_left_edge=>true, :on_right_edge=>false, :is_front_page=>false, :top_story=>false, :top_position=>false, :bottom_article=>true, :page_heading_margin_in_lines=>0, :article_bottom_spaces_in_lines=>2, :article_line_draw_sides=>"[0, 0, 0, 1]", :article_line_thickness=>0.3, :draw_divider=>nil})
->>>>>>> 2fa26ff78c73f7c56c31bb42857bc8eb143af6eb
